a1f2ef867d8a454905826bad296b45e2bf04466a
Utkarsh Lath <utkarshlath2@gmail.com>
2021-05-26 16:00:29 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, R
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
     const required = (val) => val && val.length;
     const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -114,7 +115,7 @@ import { Loading } from './LoadingComponent';
         if (dish != null)
             return(
                 <Card key={dish.id}>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                       <CardTitle>{dish.name}</CardTitle>
                       <CardText>{dish.description}</CardText>
